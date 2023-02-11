e439d9074a91a91b822edb5e67223c3fe1c3041c
faheemabbas110 <s.faheem.abbas@gmail.com>
2023-02-11 20:45:44 +0100

    file modified

--- youtube-stats/src/components/Header.js
+++ youtube-stats/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header(){
   return(
     <header className="mx-auto p-8 bg-red-700">
-      <h1 className="text-white text-4xl">YouTube like app</h1>
+      <h1 className="text-white text-4xl">Faheem app</h1>
     </header>
   )
 }
